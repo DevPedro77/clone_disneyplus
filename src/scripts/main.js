82ae29d4d27001bde4eba2f785fdf74b48ada51d
DevPedro77 <pcastro9876@gmail.com>
2024-06-07 15:46:09 -0300

    seção de planos

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('[data-tab-button]');
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(event) {
+      const abaAlvo = event.target.dataset.tabButton;
+      const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
+      removeTabs(); // Agora a função é chamada antes de 'escondeAbas()'
+      escondeAbas();
+      aba.classList.add('shows__list--is-active');
+      event.target.classList.add('shows__tabs__button--is-active');
+    });
+  }
+
+  function removeTabs() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+  }
+
+  function escondeAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    for (let i = 0; i < tabsContainer.length; i++) {
+      tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+  }
+});
