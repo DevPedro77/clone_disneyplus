430f9232bc254ab73558676201d7d885a1865e4e
DevPedro77 <pcastro9876@gmail.com>
2024-06-12 13:12:34 -0300

    finalizando o footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const buttons = document.querySelectorAll('[data-tab-button]');
+  const questions = document.querySelectorAll('[data-faq-question]')
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function(event) {
@@ -10,6 +11,17 @@ document.addEventListener('DOMContentLoaded', function () {
       aba.classList.add('shows__list--is-active');
       event.target.classList.add('shows__tabs__button--is-active');
     });
+  }
+
+  for (let i = 0; i< questions.length; i++) {
+    questions[i].addEventListener('click', abreOuFechaResposta);
+  }
+
+  function abreOuFechaResposta (elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
   }
 
   function removeTabs() {
